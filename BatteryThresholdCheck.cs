77322dd35ad8223169f97612bc8642b4764befa4
ucancallmeomi <74286342+ucancallmeomi@users.noreply.github.com>
2021-03-10 19:52:39 +0530

    Update BatteryThresholdCheck.cs

--- BatteryThresholdCheck.cs
+++ BatteryThresholdCheck.cs
@@ -1,37 +1,37 @@
-﻿using System;
+using System;
 
 namespace BatteryManagementSystem
 {
     public class BatteryThresholdCheck
     {
-        public static void checkValueLimits(string factor, float factorCurrentValue, float maxThreshold, float minThreshold)
+        public static string checkValueLimits(string factor, float factorCurrentValue, float maxThreshold, float minThreshold)
         {
+            string displayMessage = "";
             if (factorCurrentValue > maxThreshold)
-            { 
-                if(!Checker.isLanguageGerman)
-                    displayBreachLevel(factor, maxThreshold, minThreshold, "High");
-                else
-                    displayBreachLevel(factor, maxThreshold, minThreshold, "Hoch");
-            } 
+            {
+                displayMessage = Checker.isLanguageGerman ? displayBreachLevel(factor, maxThreshold, minThreshold, "Hoch") : displayBreachLevel(factor, maxThreshold, minThreshold, "High");
+                    
+            }
             else if (factorCurrentValue < minThreshold)
-                if(!Checker.isLanguageGerman)
-                    displayBreachLevel(factor, maxThreshold, minThreshold, "Low");
-                else
-                displayBreachLevel(factor, maxThreshold, minThreshold, "Niedrig");
+                displayMessage = Checker.isLanguageGerman ? displayBreachLevel(factor, maxThreshold, minThreshold, "Niedrig") : displayBreachLevel(factor, maxThreshold, minThreshold, "Low");
             else
-                if (!Checker.isLanguageGerman)
-                    Console.WriteLine("{0} : {1} is within the threshold limits - MaxThreshold: {2}, MinThreshold: {3}. No breach", factor, factorCurrentValue, maxThreshold, minThreshold);
-                else
-                    Console.WriteLine("{0} : {1} liegt innerhalb der Schwellenwerte - MaxSchwellenwert: {2}, MinSchwellenwert: {3}. Keine Verletzung", factor, factorCurrentValue, maxThreshold, minThreshold);
-        }
+                displayMessage = Checker.isLanguageGerman ? "Batterie hat keine Verletzung, alles ist normal" : "Battery has no breaches, everything is normal";
 
-        public static void displayBreachLevel(string factor, float maxThreshold, float minThreshold, string breachPoint)
+            return displayMessage;
+        }
+        public static string displayBreachLevel(string factor, float maxThreshold, float minThreshold, string breachPoint)
         {
-            if(!Checker.isLanguageGerman)
-                Console.WriteLine("{0} breached, level {1}. Range is {2} - {3}.", factor, breachPoint, minThreshold, maxThreshold);
-            else
-                Console.WriteLine("{0} verletzt, niveau {1}. Reichweite is {2} - {3}.", factor, breachPoint, minThreshold, maxThreshold);
-        }
+            string outputMessage = "";
 
+            if (!Checker.isLanguageGerman)
+            {
+                outputMessage = string.Format("{0} breached, level {1}. Range is {2} - {3}.", factor, breachPoint, minThreshold, maxThreshold);
+            }
+            else
+            {
+                outputMessage = string.Format("{0} verletzt, niveau {1}. Reichweite is {2} - {3}.", factor, breachPoint, minThreshold, maxThreshold);
+            }
+            return outputMessage;
+        }
     }
 }
