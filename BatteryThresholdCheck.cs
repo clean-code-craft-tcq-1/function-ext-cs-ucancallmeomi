97a441f828c60a25831f258feb614ec4c5bd76ca
ucancallmeomi <74286342+ucancallmeomi@users.noreply.github.com>
2021-03-10 20:27:36 +0530

    Reduced CCN

--- BatteryThresholdCheck.cs
+++ BatteryThresholdCheck.cs
@@ -1,37 +1,30 @@
 using System;
+using System.Collections.Generic;
 
 namespace BatteryManagementSystem
 {
     public class BatteryThresholdCheck
     {
-        public static string checkValueLimits(string factor, float factorCurrentValue, float maxThreshold, float minThreshold)
-        {
-            string displayMessage = "";
-            if (factorCurrentValue > maxThreshold)
-            {
-                displayMessage = Checker.isLanguageGerman ? displayBreachLevel(factor, maxThreshold, minThreshold, "Hoch") : displayBreachLevel(factor, maxThreshold, minThreshold, "High");
-                    
-            }
-            else if (factorCurrentValue < minThreshold)
-                displayMessage = Checker.isLanguageGerman ? displayBreachLevel(factor, maxThreshold, minThreshold, "Niedrig") : displayBreachLevel(factor, maxThreshold, minThreshold, "Low");
-            else
-                displayMessage = Checker.isLanguageGerman ? "Batterie hat keine Verletzung, alles ist normal" : "Battery has no breaches, everything is normal";
 
-            return displayMessage;
-        }
-        public static string displayBreachLevel(string factor, float maxThreshold, float minThreshold, string breachPoint)
+        public static void checkValueLimits(string factor, float factorCurrentValue, float maxThreshold, float minThreshold)
         {
-            string outputMessage = "";
+            if (factorCurrentValue > maxThreshold)
+                displayBreachLevel(factor, maxThreshold, minThreshold, "High");
+            else if (factorCurrentValue < minThreshold)
+                displayBreachLevel(factor, maxThreshold, minThreshold, "Low");
+        }
+
+        public static void displayBreachLevel(string factor, float maxThreshold, float minThreshold, string breachPoint)
+        {
+            Dictionary<string, string> breachValues = new Dictionary<string, string>();
+            breachValues.Add("High", "Hoch");
+            breachValues.Add("Low", "Niedrig");
 
             if (!Checker.isLanguageGerman)
-            {
-                outputMessage = string.Format("{0} breached, level {1}. Range is {2} - {3}.", factor, breachPoint, minThreshold, maxThreshold);
-            }
+                Console.WriteLine("{0} breached, level {1}. Range is {2} - {3}.", factor, breachPoint, minThreshold, maxThreshold);
             else
-            {
-                outputMessage = string.Format("{0} verletzt, niveau {1}. Reichweite is {2} - {3}.", factor, breachPoint, minThreshold, maxThreshold);
-            }
-            return outputMessage;
+                Console.WriteLine("{0} verletzt, Niveau {1}. Reichweite is {2} - {3}.", factor, breachValues[breachPoint], minThreshold, maxThreshold);
         }
+
     }
 }
